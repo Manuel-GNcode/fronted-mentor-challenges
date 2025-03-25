6ac154e85eb0e493c9884658b973318661f3dec7
GNcode <gngalarga027@gmail.com>
2025-03-25 08:20:43 -0500

    Add social media icons and update header layout with new favicon

--- sunnyside-agency-landing-page-main/src/App.jsx
+++ sunnyside-agency-landing-page-main/src/App.jsx
@@ -4,9 +4,15 @@ function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <header className="bg-black flex justify-between mt-8 px-8">
+      <img src="logo.svg" alt="logo" />
+      <button>
+        <img src="icon-hamburger.svg" alt="hamburger" />
+      </button>
+    </header>
+    <main>
+      <h1 className="font-fraunces font-black text-4xl">WE ARE CREATIVES</h1>
+    </main>
     </>
   )
 }
